ab5dd7aa279d7359bf9ced451cbb350e02e88a4d
Andreja Jevtic <andrajevtic@gmail.com>
2018-12-05 14:39:11 +0100

    first try

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,25 @@
-'use strict';
-
 module.exports = {
   extends: [
     'eslint-config-airbnb-base',
   ].map(require.resolve),
   parserOptions: {
-    sourceType: 'script',
-    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaVersion: 9,
   },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@config', './lib/config'],
+          ['@db-models', './lib/database/models'],
+          ['@graphql-models', './lib/graphql/models'],
+          ['@utils', './lib/utils'],
+          ['@services', './lib/services'],
+        ],
+        extensions: ['.js','.json']
+      }
+    }
+  }
   // overrides: [
   //   {
   //     files: ['**/*.test.js'],
